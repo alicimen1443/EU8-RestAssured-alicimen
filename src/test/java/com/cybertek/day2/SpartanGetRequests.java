72dcaafa089dba401190afe10f08e56b9164bcc5
alicimen <alicimen1443@gmail.com>
2022-07-20 15:53:19 +0300

    ready for first testcase

---
+++ src/test/java/com/cybertek/day2/SpartanGetRequests.java
@@ -0,0 +1,13 @@
+package com.cybertek.day2;
+
+public class SpartanGetRequests {
+
+    String url = "http://54.144.126.242:8000/api/spartans";
+
+    //Given Accept type application/xml
+    //When user send GET request to api/spartans end point
+    //Then response Content Type must be application/xml
+    //And response body should include spartan result
+
+
+}
