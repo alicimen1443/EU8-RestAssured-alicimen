8cbc0b4b2a66cf8a8d84ade8ab3d4d855a9f4913
alicimen <alicimen1443@gmail.com>
2022-07-20 19:26:19 +0300

    task is done HR

--- src/test/java/com/cybertek/day2/HrGetRequests.java
+++ src/test/java/com/cybertek/day2/HrGetRequests.java
@@ -1,7 +1,9 @@
 package com.cybertek.day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -34,6 +36,24 @@ public class HrGetRequests {
         and content type equals to application/json
         and response body contains Americas
      */
+
+    @DisplayName("GET request to /regions/2 ")
+    @Test
+    public void test2(){
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when().get("/regions/2");
+
+        //verify status code
+        Assertions.assertEquals(200,response.statusCode());
+
+        //verify content type
+        Assertions.assertEquals("application/json",response.contentType());
+
+        response.prettyPrint();
+
+        //verify body contains Americas
+        Assertions.assertTrue(response.body().asString().contains("Americas"));
+    }
 
 
 }
