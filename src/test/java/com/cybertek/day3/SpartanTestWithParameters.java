f1ff2d79acfa67939faa24156b45e152a8cda979
alicimen <alicimen1443@gmail.com>
2022-07-22 10:43:58 +0300

    day3: pathParam test1

--- src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
+++ src/test/java/com/cybertek/day3/SpartanTestWithParameters.java
@@ -1,6 +1,10 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.baseURI;
 
@@ -14,4 +18,39 @@ public class SpartanTestWithParameters {
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI="http://54.144.126.242:8000";
     }
+
+    /*   Given accept type is Json
+          And Id parameter value is 5
+          When user sends GET request to /api/spartans/{id}
+          Then response status code should be 200
+          And response content-type: application/json
+          And "Blythe" should be in response payload (response body and response payload are the same thing)
+       */
+
+
+    @DisplayName("GET request to /api/spartans/{id} with ID 5")
+    @Test
+    public void test1(){
+
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id",5)
+                            .when()
+                                    .get("/api/spartans/{id}"); // since I use baseURI I am just typing endpoint here
+
+
+        //verify status code
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+        //verify Blythe in the json payload/body
+        assertTrue(response.body().asString().contains("Blythe"));
+
+
+
+    }
+
+
+
+
+
 }
