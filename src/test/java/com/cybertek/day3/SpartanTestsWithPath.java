bbf923ec46be264c9b5c8b67a598358b1d8ebd6d
alicimen <alicimen1443@gmail.com>
2022-07-22 23:31:46 +0300

    day3: SpartanTestsWithPath test1

--- src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
+++ src/test/java/com/cybertek/day3/SpartanTestsWithPath.java
@@ -1,5 +1,7 @@
 package com.cybertek.day3;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,9 +18,51 @@ public class SpartanTestsWithPath {
         baseURI="http://54.144.126.242:8000";
     }
 
+    /*
+     Given accept type is json
+     And path param id is 10
+     When user sends a get request to "api/spartans/{id}"
+     Then status code is 200
+     And content-type is "application/json"
+     And response payload values match the following:
+          id is 10,
+          name is "Lorenza",
+          gender is "Female",
+          phone is 3312820936
+   */
+
     @DisplayName("GET one spartan with Path Method")
     @Test
     public void test1(){
+        Response response = given().accept(ContentType.JSON)
+                .and().pathParam("id", 10)
+                .when().get("/api/spartans/{id}");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.contentType());
+
+        //verify each json key has specific value
+        System.out.println(response.path("id").toString());
+        System.out.println(response.path("name").toString());
+        System.out.println(response.path("gender").toString());
+        System.out.println(response.path("phone").toString());
+
+        int id = response.path("id");
+        String name = response.path("name");
+        String gender = response.path("gender");
+        long phone = response.path("phone");
+
+        System.out.println("id = " + id);
+        System.out.println("phone = " + phone);
+        System.out.println("gender = " + gender);
+        System.out.println("name = " + name);
+
+        //assert the values
+        assertEquals(10,id);
+        assertEquals("Lorenza",name);
+        assertEquals("Female",gender);
+        assertEquals(3312820936l,phone);
+
 
     }
 
