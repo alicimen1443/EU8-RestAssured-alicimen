3749ca9c9449fdddcb48d7f2cb33c19848542a92
alicimen <alicimen1443@gmail.com>
2022-07-23 22:32:41 +0300

    day4: ORDSApiWithJsonPath added

--- src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -34,10 +34,42 @@ public class ORDSApiWithJsonPath extends HRTestBase {
         List<String> allCountryIds = jsonPath.getList("items.country_id");
         System.out.println(allCountryIds);
 
+        //get all country names where their region id is equal to 2
+        //inside the curly braces it is like if condition.After condition, it is my value
+        //this is called groovy Jpath syntax
+        List<String> countryNameWithRegionId2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
+        System.out.println(countryNameWithRegionId2);
+    }
 
+    @DisplayName("GET request/employees with query param")
+    @Test
+    public void test2(){
+        //we added limit query param to get 107 employees
+        Response response = given().queryParam("limit", 107)
+                .when().get("/employees");
+
+        //get me all email of employees who is working as IT_PROG
+        JsonPath jsonPath = response.jsonPath();
+
+        List<String> employeeITProgs = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.email");
+        System.out.println(employeeITProgs);
+
+        //get me first name of employees who is making more than 10000
+        List<String > empNames = jsonPath.getList("items.findAll {it.salary>10000}.first_name");
+        System.out.println(empNames);
+
+        //get the max salary first_name
+        String kingFirstName = jsonPath.getString("items.max {it.salary}.first_name");
+        System.out.println("kingFirstName = " + kingFirstName);
+
+        //this groovy Jpath syntax is independent of jsonPath it also works with response path
+        String kingFirstName2 = response.path("items.max {it.salary}.first_name");
+        System.out.println("kingFirstName2 = " + kingFirstName2);
 
 
     }
+
+
 
 
 
