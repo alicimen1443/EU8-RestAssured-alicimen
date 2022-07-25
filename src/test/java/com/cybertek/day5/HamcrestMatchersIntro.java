ac580a6b541cdc860f3d2e641dd35b466d486673
alicimen <alicimen1443@gmail.com>
2022-07-25 13:39:52 +0300

    day5 : HamcrestMatchersIntro.java  done

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -1,8 +1,12 @@
 package com.cybertek.day5;
 
+
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
@@ -36,37 +40,53 @@ public class HamcrestMatchersIntro {
         assertThat(5 + 5, is(greaterThan(9)));
     }
 
-        @DisplayName("Assertion with String")
-        @Test
-        public void stringHamcrest(){
-            String text = "B22 is learning Hamcrest";
+    @DisplayName("Assertion with String")
+    @Test
+    public void stringHamcrest() {
+        String text = "B22 is learning Hamcrest";
 
-            //checking for equaltiy is same as numbers
-            assertThat(text,is("B22 is learning Hamcrest"));
-            assertThat(text,equalTo("B22 is learning Hamcrest"));
-            assertThat(text,is(equalTo("B22 is learning Hamcrest")));
+        //checking for equaltiy is same as numbers
+        assertThat(text, is("B22 is learning Hamcrest"));
+        assertThat(text, equalTo("B22 is learning Hamcrest"));
+        assertThat(text, is(equalTo("B22 is learning Hamcrest")));
 
-            //check if this text starts with B22
-            assertThat(text,startsWith("B22"));
-            //now do it in case insensitive manner
-            assertThat(text,startsWithIgnoringCase("b22"));
-            //endswith
-            assertThat(text,endsWith("rest"));
+        //check if this text starts with B22
+        assertThat(text, startsWith("B22"));
+        //now do it in case insensitive manner
+        assertThat(text, startsWithIgnoringCase("b22"));
+        //endswith
+        assertThat(text, endsWith("rest"));
 
-            //check if text contains String learning
-            assertThat(text,containsString("learning"));
-            //with ignoring case
-            assertThat(text,containsStringIgnoringCase("LEARNING"));
+        //check if text contains String learning
+        assertThat(text, containsString("learning"));
+        //with ignoring case
+        assertThat(text, containsStringIgnoringCase("LEARNING"));
 
-            String str = "  ";
-            //check if above str is blank
-            assertThat(str,blankString());
-            //check if trimmed str is empty
-            assertThat(str.trim(),emptyString());
+        String str = "  ";
+        //check if above str is blank
+        assertThat(str, blankString());
+        //check if trimmed str is empty
+        assertThat(str.trim(), emptyString());
 
+    }
 
+    @DisplayName("Hamcrest for Collection")
+    @Test
+    public void testCollection() {
 
+        List<Integer> listOfNumbers = Arrays.asList(1, 4, 5, 6, 32, 54, 66, 77, 45, 23);
 
-        }
+        //check size of list
+        assertThat(listOfNumbers, hasSize(10));
+
+        //check if this list hasItem 77
+        assertThat(listOfNumbers,hasItem(77));
+        //check if this list hasItems 77,54,23
+        assertThat(listOfNumbers,hasItems(77,54,23));
+        //check if all numbers greater than 0
+        assertThat(listOfNumbers,everyItem(greaterThan(0)));
+
+    }
+
 
 }
