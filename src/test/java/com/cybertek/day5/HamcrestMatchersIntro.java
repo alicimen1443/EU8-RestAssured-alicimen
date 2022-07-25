1d989b92fd4d808a84f951d089d9d8585ddc6a71
alicimen <alicimen1443@gmail.com>
2022-07-25 11:14:11 +0300

    day5 : HamcrestMatchersIntro.java  number examples

--- src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersIntro.java
@@ -10,9 +10,27 @@ import static org.hamcrest.Matchers.*;
 public class HamcrestMatchersIntro {
 
     @Test
-    public void simpleTest(){
+    public void simpleTest() {
 
-       assertThat(5+5,is(10));
+        //actual 5+5
+        assertThat(5 + 5, is(10));
+        assertThat(5 + 5, equalTo(10));
+        //Matchers has 2 overloaded version
+        //first that accept actual value
+        //second that accept another matchers
+        //below examples is method is accepting another matchers equal to make it readable
+        assertThat(5 + 5, is(equalTo(10)));
+        assertThat(5+5,not(9));
+        //or u can say
+        assertThat(5+5,is(not(9)));
+        //or
+        assertThat(5+5,is(not(equalTo(9))));
 
+        //number comparison
+        //greaterThan()
+        //greaterThanOrEqualTo()
+        //lessThan()
+        //lessThanOrEqualTo()
+        assertThat(5+5,is(greaterThan(9)));
     }
 }
