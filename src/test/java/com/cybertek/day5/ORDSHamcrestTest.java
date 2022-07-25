10c19331a327b64690673ea720b0d01de1882505
alicimen <alicimen1443@gmail.com>
2022-07-25 18:24:50 +0300

    day5 : ORDSHamcrestTest.java added

--- src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
+++ src/test/java/com/cybertek/day5/ORDSHamcrestTest.java
@@ -1,8 +1,16 @@
 package com.cybertek.day5;
 
 import com.cybertek.utilities.HRTestBase;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
 
 public class ORDSHamcrestTest extends HRTestBase {
 
@@ -13,7 +21,35 @@ public class ORDSHamcrestTest extends HRTestBase {
         //verify each job_id is IT_PROG
         //verify first names are.... (find proper method to check list against list)
         //verify emails without checking order(provide emails in different order,just make sure it has some emails)
+        //expected names
 
+        List<String> names = Arrays.asList("Alexander","Bruce","David","Valli","Diana");
+
+
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .when()
+                .get("/employees")
+                .then()
+                .statusCode(200)
+                .body("items.job_id",everyItem(equalTo("IT_PROG")))
+                .body("items.first_name",containsInRelativeOrder("Alexander","Bruce","David","Valli","Diana")) //contains with order
+                .body("items.email",containsInAnyOrder("VPATABAL","DAUSTIN","BERNST","AHUNOLD","DLORENTZ")) //contains without order
+                .body("items.first_name", equalTo(names)); // equality of lists assertion
+
+    }
+
+    @Test
+    public void employeesTest2(){
+        //we want to chain and also get response object
+
+        given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .when()
+                .get("/employees")
+                .then()
+                .statusCode(200)
+                .body("items.job_id",everyItem(equalTo("IT_PROG")));
 
 
     }
