d7e8a9061fc2f0876bf0b2ad78c4fe9ca05fdcf0
alicimen <alicimen1443@gmail.com>
2022-07-25 14:37:05 +0300

    day5 : HamcrestMatchersApiTest.java first test case done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -1,5 +1,6 @@
 package com.cybertek.day5;
 
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,6 +13,19 @@ public class HamcrestMatchersApiTest {
     @DisplayName("OneSpartan with Hamcrest and chaining")
     @Test
     public void test1(){
+
+        given().accept(ContentType.JSON)
+                .and().pathParam("id",15)
+                .when().get("\n" +
+                        "http://54.144.126.242:8000/api/spartans/{id}")
+                .then().statusCode(200)
+                .and()
+                .contentType("application/json")
+                .and()
+                .body("id",equalTo(15),"name",is("Meta")
+                ,"gender",is("Female"),"phone",is(1938695106));
+
+
 
     }
 
