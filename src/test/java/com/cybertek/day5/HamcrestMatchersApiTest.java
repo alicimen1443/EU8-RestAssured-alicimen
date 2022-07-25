0ba250d58e63035176ca68a3084479b4d4cc7d79
alicimen <alicimen1443@gmail.com>
2022-07-25 16:18:46 +0300

    day5 : HamcrestMatchersApiTest.java second test case done

--- src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
+++ src/test/java/com/cybertek/day5/HamcrestMatchersApiTest.java
@@ -14,6 +14,19 @@ public class HamcrestMatchersApiTest {
     @Test
     public void test1(){
 
+         /*
+       given accept type is Json
+       And path param id is 15
+       When user sends a get request to spartans/{id}
+       Then status code is 200
+       And content type is Json
+       And json data has following
+           "id": 15,
+           "name": "Meta",
+           "gender": "Female",
+           "phone": 1938695106
+        */
+
         given().accept(ContentType.JSON)
                 .and().pathParam("id",15)
                 .when().get("\n" +
@@ -25,9 +38,32 @@ public class HamcrestMatchersApiTest {
                 .body("id",equalTo(15),"name",is("Meta")
                 ,"gender",is("Female"),"phone",is(1938695106));
 
-
-
     }
+
+    @DisplayName("CBTraining Teacher requset with chaining and matchers")
+    @Test
+    public void teacherData(){
+
+        // given , when , then it is similar to gerkhin language actually. It is BDD design
+
+            given().accept(ContentType.JSON)
+                    .and()
+                    .pathParam("id",21887)
+                    .when().get("http://api.cybertektraining.com/teacher/{id}")
+                    .then()  // after than it is response part
+                    .statusCode(200)
+                    .and()
+                    .contentType("application/json;charset=UTF-8")
+                    .and()
+                    .header("Content-Length",is("275"))
+                    .and().assertThat()
+                    .header("Date",notNullValue())
+                    .and().assertThat()
+                    .body("teachers[0].firstName",is("Leonel"))
+                    .body("teachers[0].lastName",is("Messi"))
+                    .body("teachers[0].gender",equalTo("Male"));
+    }
+
 
 
 }
