40a14ece4e682e6a0c5b4d38d9bf76ce00fa8d48
alicimen <alicimen1443@gmail.com>
2022-07-26 16:21:56 +0300

    day5 : DBUtils.java added

---
+++ src/test/java/com/cybertek/day5/SpartanAPIvsDB.java
@@ -0,0 +1,12 @@
+package com.cybertek.day5;
+
+import com.cybertek.utilities.SpartanTestBase;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class SpartanAPIvsDB extends SpartanTestBase {
+
+
+}
--- src/test/java/com/cybertek/utilities/SpartanTestBase.java
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -1,5 +1,6 @@
 package com.cybertek.utilities;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
 import static io.restassured.RestAssured.baseURI;
@@ -11,6 +12,17 @@ public abstract class SpartanTestBase {
         //save baseurl inside this variable so that we don't need to type each http method.
         baseURI="http://54.144.126.242:8000";
 
-        //get IP adress from configurations
+        String dbUrl = "jdbc:oracle:thin:@54.144.126.242:1521:xe";
+        String dbUsername = "SP";
+        String dbPassword = "SP";
+
+        //DBUtils.createConnection(dbUrl,dbUsername,dbPassword);
     }
+
+    @AfterAll
+    public static void teardown(){
+
+        //DBUtils.destroy();
+    }
+
 }
