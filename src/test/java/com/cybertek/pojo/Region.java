674d2ede7bf0b2b9163144598dcdfe0068e286f1
alicimen <alicimen1443@gmail.com>
2022-07-26 23:33:47 +0300

    day6 : region class updated with @JsonProperty annotation

--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -1,43 +1,22 @@
 package com.cybertek.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import java.util.List;
 
+@Getter
+@Setter
+@ToString
 public class Region {
-
-    private int region_id;
+    //region_id
+    //if your jsonkey and variable name not matching, you can map it with jsonProperty
+    @JsonProperty("region_id")//this json property annotation takes region_id field and puts it inot regionId variable.The logic here is to handle the variable names that contains spaces
+    private int regionId;
     private String region_name;
     private List<Link> links;
 
-    public int getRegion_id() {
-        return region_id;
-    }
 
-    public void setRegion_id(int region_id) {
-        this.region_id = region_id;
-    }
-
-    public String getRegion_name() {
-        return region_name;
-    }
-
-    public void setRegion_name(String region_name) {
-        this.region_name = region_name;
-    }
-
-    public List<Link> getLinks() {
-        return links;
-    }
-
-    public void setLinks(List<Link> links) {
-        this.links = links;
-    }
-
-    @Override
-    public String toString() {
-        return "Region{" +
-                "region_id=" + region_id +
-                ", region_name='" + region_name + '\'' +
-                ", links=" + links +
-                '}';
-    }
 }
