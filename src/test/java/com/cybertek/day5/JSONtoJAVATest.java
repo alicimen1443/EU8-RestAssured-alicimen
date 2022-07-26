94dd7cb9759f14c2eb985ce9e5a2befe87643913
alicimen <alicimen1443@gmail.com>
2022-07-26 13:33:55 +0300

    day5 : Ojdbc added

--- src/test/java/com/cybertek/day5/JSONtoJAVATest.java
+++ src/test/java/com/cybertek/day5/JSONtoJAVATest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -31,11 +32,26 @@ public class JSONtoJAVATest extends SpartanTestBase {
         String actualName = (String) jsonMap.get("name");
         assertThat(actualName,is("Meta"));// here we used hamcrest assertion. If you want to use Junit assertion you can say AssertEquals()
 
-
-
-
-
     }
+
+        @DisplayName("GET all spartans to JAVA data structure")
+        @Test
+        public void getAllSpartan(){
+
+            Response response = get("/api/spartans").then().statusCode(200).extract().response();
+
+            //we need to convert json to java which is deserialize
+
+            List<Map<String,Object>> jsonList = response.as(List.class);
+
+            System.out.println("jsonList.get(1).get(\"name\") = " + jsonList.get(1).get("name"));
+
+            Map<String,Object> spartan3 = jsonList.get(2);
+            System.out.println(spartan3);
+
+
+        }
+
 
 
 
