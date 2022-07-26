9a81ecf9a6c2ff9bfcc0a045c726296a05be752f
alicimen <alicimen1443@gmail.com>
2022-07-26 20:00:49 +0300

    day6 : ORDSPojoGetRequestTest.javajava added

---
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -0,0 +1,18 @@
+package com.cybertek.day6;
+
+import com.cybertek.utilities.HRTestBase;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class ORDSPojoGetRequestTest extends HRTestBase {
+
+    @Test
+    public void regionTest(){
+
+    }
+
+
+}
