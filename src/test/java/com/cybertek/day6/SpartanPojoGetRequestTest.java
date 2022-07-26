4dd7458f3b7f37e501b37ee47b0db69955c9eea4
alicimen <alicimen1443@gmail.com>
2022-07-26 17:18:10 +0300

    day6 : Spartan.java second way added

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -3,6 +3,7 @@ package com.cybertek.day6;
 import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -27,11 +28,25 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         //Deserialize--> Json to POJO(java custom class)
         //2 diferent way to do this
         //1.using as() method
+        //we convert json response to spartan object with the help of jackson
+        //as() method uses jackson to deserialize(converting JSON to Java class)
         Spartan spartan15 = response.as(Spartan.class);
         System.out.println(spartan15);
         System.out.println("spartan15.getId() = " + spartan15.getId());
         System.out.println("spartan15.getGender() = " + spartan15.getGender());
 
+        //second way of deserialize json to java
+        //2.using JsonPath to deserialize to custom class
+        //below 2 line is doing literally the same job as response.as() method
+        JsonPath jsonPath = response.jsonPath();
+
+         //this path is empty.The reason is I am already getting one spartan object from the json response
+        //I can directly convert it to the spartan class
+        Spartan s15 = jsonPath.getObject("", Spartan.class);
+
+        System.out.println(s15);
+        System.out.println("s15.getName() = " + s15.getName());
+        System.out.println("s15.getPhone() = " + s15.getPhone());
 
     }
 
