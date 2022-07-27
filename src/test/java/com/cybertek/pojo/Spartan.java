cc8a1e36c8b5e4d5e46e4eedcb3897077fc6cc9d
alicimen <alicimen1443@gmail.com>
2022-07-27 15:37:27 +0300

    day7 : SpartanPostRequestDemo third way

--- src/test/java/com/cybertek/pojo/Spartan.java
+++ src/test/java/com/cybertek/pojo/Spartan.java
@@ -9,6 +9,16 @@ package com.cybertek.pojo;
 }
  */
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+
+@JsonIgnoreProperties(value = "id",allowSetters = true)
 public class Spartan {
     //getter setter
     //toString
@@ -18,45 +28,4 @@ public class Spartan {
     private long phone;
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public long getPhone() {
-        return phone;
-    }
-
-    public void setPhone(long phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "Spartan{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", gender='" + gender + '\'' +
-                ", phone=" + phone +
-                '}';
-    }
 }
