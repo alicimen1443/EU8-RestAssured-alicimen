9b4eedc930ee43fb23777c06f4a6cb9122962640
alicimen <alicimen1443@gmail.com>
2022-07-27 10:37:03 +0300

    day6 : Rgions task is done

--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -1,5 +1,6 @@
 package com.cybertek.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,13 +11,15 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Region {
     //region_id
     //if your jsonkey and variable name not matching, you can map it with jsonProperty
     @JsonProperty("region_id")//this json property annotation takes region_id field and puts it inot regionId variable.The logic here is to handle the variable names that contains spaces
     private int regionId;
     private String region_name;
-    private List<Link> links;
+
+   // private List<Link> links;
 
 
 }
